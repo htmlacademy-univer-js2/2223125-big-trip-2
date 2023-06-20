4ca760fce2031e4bc56495affde1270423ab257c
konfessor <66369760+konfessor@users.noreply.github.com>
2023-06-20 19:22:11 +0500

    added sort

--- src/view/point-editing.js
+++ src/view/point-editing.js
@@ -154,7 +154,7 @@ export default class PointEditingView extends AbstractView {
 
   setFormSubmitHandler = (callback) => {
     this._callback.formSubmit = callback;
-    this.element.querySelector('form').addEventListener('click', this.#formSubmitHandler);
+    this.element.querySelector('.event__save-btn').addEventListener('click', this.#formSubmitHandler);
   };
 
   #formSubmitHandler = (evt) => {
